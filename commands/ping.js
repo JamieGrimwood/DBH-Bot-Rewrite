897470f0960f4bec18c23a0c79a90f5a8a0199a6
lolollllo <99907738+lolollllo@users.noreply.github.com>
2022-04-01 21:14:04 +0500

    Update ping.js
    Same thing as github.com/DanBot-Hosting/DanBotHostingStats/blob/master/bot/discord/commands/ping.js
    with the v13 changes.

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,10 @@
+const Discord = require('discord.js');
+
 exports.run = (client, message, args) => {
-    message.channel.send("pong!").catch(console.error);
-}
+    let embed = new Discord.MessageEmbed()
+        .setColor("RED")
+        .setTitle("DanBot Hosting - Ping")
+        .setDescription(`API Latency: ${Math.round(client.ws.ping)}ms`)
+        .setTimestamp()
+    message.channel.send({ embeds: [embed] })
+};
