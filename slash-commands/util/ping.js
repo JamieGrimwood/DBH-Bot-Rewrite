0d9e239c0601b8c7068721823549a1239e55710c
lolollllo <99907738+lolollllo@users.noreply.github.com>
2022-04-01 21:12:08 +0500

    Update ping.js
    Same thing from github.com/DanBot-Hosting/DanBotHostingStats/blob/master/bot/discord/commands/ping.js
    Just as a slash command with yk v13 changes. trying to help. 🙄 👍

--- slash-commands/util/ping.js
+++ slash-commands/util/ping.js
@@ -1,10 +1,16 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
+	async execute(interaction, client) {
+		let embed = new Discord.MessageEmbed()
+		  .setColor('RED') 
+		  .setTitle('DanBot Hosting - Ping') 
+		  .setDescription(`API Latency: ${Math.round(client.ws.ping)}ms`)
+		  .setTimestamp()
+	       await interaction.reply({ embeds: [embed] });
 	},
 };
